29699ddafe9f20ddda10019b7a777c508f1eec87
Ashok Dey <ashokdey100@gmail.com>
2018-03-02 16:18:44 +0530

    --update : webpack now resolves both JSX and JS

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -27,5 +27,8 @@ module.exports = {
         }
       }
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-console.log('hello from index.js');
-(async () => {
-  await console.log('hello');
-})();
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
