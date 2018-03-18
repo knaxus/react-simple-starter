47e51175d41fde15e92bbedcf5c30fb8781368ce
Ashok Dey <ashokdey100@gmail.com>
2018-03-18 22:32:51 +0530

    --update : redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './app/App';
+import rootReducer from './reducer';
+
+const initialState = {};
 /* eslint-disable */
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// If Redux DevTools Extension is installed use it, otherwise use Redux compose
+const store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+/* eslint-enable react/jsx-filename-extension */
 
 if (module.hot) {
   module.hot.accept('./app/App', () => {
     const NextApp = require('./app/App').default;
-    ReactDOM.render(<NextApp />, document.getElementById('root'));
+    ReactDOM.render(
+      <Provider store={store}>
+        <NextApp />
+      </Provider>,
+      document.getElementById('root'),
+    );
   });
 }
 /* eslint-enable */
